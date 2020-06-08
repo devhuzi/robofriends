eb1a4f1fe3c43d8243c50e030cdc352feb148bf2
DevHuzi <hux3fa@gmail.com>
2020-06-08 12:38:33 +1000

    Updates

---
+++ precache-manifest.9412741578265c75e7ab548a8ba49d0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11600e930ea595cb8a00d5653ac344be",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6fd7b08a33e5de2bbefe",
+    "url": "/robofriends/static/css/main.0d3232c6.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd7b08a33e5de2bbefe",
+    "url": "/robofriends/static/js/main.cd4f4e4b.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "9c7253337eedeec26ce1af09b063f770",
+    "url": "/robofriends/static/media/Mightyspidey-pmaa.9c725333.ttf"
+  }
+]);
